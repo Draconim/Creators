5628f22f65908424ee71b70fad2f5c8de18884ed
bPet75 <bpeti437@gmail.com>
2022-03-11 19:35:15 +0100

    Events tábla létrehozása

---
+++ App/SportCheck/database/migrations/2022_03_11_182728_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('ID');
+            $table->text('description');
+            $table->datetime('date');
+            $table->time('duration');
+            $table->string('code');
+            $table->datetime('check-in_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
