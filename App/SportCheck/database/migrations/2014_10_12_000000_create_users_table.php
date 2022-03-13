4928f0c098965cae84805c84b696700992cea0a6
bPet75 <83027074+bPet75@users.noreply.github.com>
2022-03-13 09:23:54 +0100

    Táblák előkészítése

--- App/SportCheck/database/migrations/2014_10_12_000000_create_users_table.php
+++ App/SportCheck/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,22 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('department');
+            $table->string('neptun_code', 6);
+            $table->unsignedInteger('address_id');
+            $table->unsignedInteger('role_id');
+
+            //A külső kulcsok msot nem kellenek, mert it csak ez az egy tábla lesz.
+
+            //$table->foreign('role_id')->references('id')->on('roles');
+            //$table->foreign('address_id')->references('id')->on('addresses');
         });
     }
 
