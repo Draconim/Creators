8df85a27734f782dfad00b6bbdce9a8f5231d9ac
Barankai László Ferenc <barankailaci@gmail.com>
2022-03-29 22:16:46 +0200

    Új rendezvény frontend elkészítve

--- App/SportCheck/resources/views/events/Create.blade.php
+++ App/SportCheck/resources/views/events/Create.blade.php
@@ -1,32 +1,46 @@
 @extends('home')
 @section('content')
  
-<div class="card">
-  <div class="card-header">Contactus Page</div>
-  <div class="card-body">
+<div class="container">
+  <div class="card">
+    <div class="card-header text-center">Új rendezvény</div>
+    <div class="card-body">
 
-      <form action="{{ url('events') }}" method="post">
-        @csrf
-        <label>Name</label></br>
-        <input type="text" name="name" id="name" class="form-control"></br>
-        <label>Description</label></br>
-        <input type="text" name="description" id="description" class="form-control"></br>
+        <form action="{{ url('events') }}" method="post">
+          @csrf
 
-        <label>Date</label></br>
-        <input type="text" name="date" id="date" class="form-control"></br>
+          <div class="container d-flex flex-column justify-content-center align-items-center">
+            <div class="col-8">
+              <label for="name">Rendezvény neve</label>
+              <input type="text" name="name" id="name" class="form-control mb-4">
+            </div>
+            <div class="col-8">
+              <label for="desc">Leírás</label>
+              <input type="text" name="desc" id="description" class="form-control mb-4">
+            </div>
+            <div class="col-8">
+              <label for="date">Időpont</label>
+              <input type="text" name="date" id="date" class="form-control mb-4" placeholder="YYYY-MM-DD ÓÓ:PP:MM">
+            </div>
+            <div class="col-8">
+              <label for="dur">Időtartam</label>
+              <input type="text" name="dur" id="duration" class="form-control mb-4" placeholder="ÓÓ:PP:MM">
+            </div>
+            <div class="col-8">
+              <label for="checkin">Bejelentkezés ideje</label>
+              <input type="text" name="checkin" id="check_in_time" class="form-control mb-2" placeholder="YYYY-MM-DD ÓÓ:PP:MM">
+            </div>
+            
+            
+            <input type="submit" value="Rendezvény mentése" class="btn btn-success btn-lg m-4 col-4">
+            
+          </div>
+          <input type="hidden" name="code" id="code">
 
-        <label>Duration</label></br>
-        <input type="text" name="duration" id="duration" class="form-control"></br>
-
-        <label>Check-in time</label></br>
-        <input type="text" name="check_in_time" id="check_in_time" class="form-control"></br>
-       
-        <input type="hidden" name="code" id="code">
-
-        <input type="submit" value="Save" class="btn btn-success"></br>
-    </form>
-   
+          
+      </form>
+    
+    </div>
   </div>
 </div>
- 
 @endsection
