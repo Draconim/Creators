b2876a851f43a628b06ef6f306c3443b99c517c5
Barankai László Ferenc <barankailaci@gmail.com>
2022-03-29 22:17:35 +0200

    Admin gombok (módosítás, letöltés) hozzáadva event oldalhoz

--- App/SportCheck/resources/views/events/events.blade.php
+++ App/SportCheck/resources/views/events/events.blade.php
@@ -4,19 +4,39 @@
 <div class="d-flex flex-column text-center">
     <a href="{{route('create_event')}}" class="d-flex align-self-center btn btn-primary btn-m col-sm-1 m-4"> Új rendezvény hozzáadása</a>
     @foreach($events as $item)
-        <div class="card d-flex mb-3 col-sm-10 align-self-center">
-            <h5 class="card-header">{{ $item->name }}</h5>
-            <div class="card-body d-flex align-items-center justify-content-center flex-row">
+        <div class="d-flex flex-column">
+            <div class="d-flex flex-row justify-content-center">
                 <div class="d-flex">
-                    <label for="date" class="d-flex align-self-baseline"><h6>Rendezvény ideje:&nbsp</h6></label>
-                    <div name="date" class="d-flex align-self-baseline">
-                        {{ $item->date }}
+                    <div class="d-flex mx-2">
+                        <a href="{{url('/events/'.$item->id.'/update/')}}" class="btn btn-primary align-self-center mb-1">Módosítás</a>   
                     </div>
                 </div>
-                
+                <div class="d-flex">
+                    <div class="d-flex mx-2">
+                        <a href="{{url('/events/'.$item->id.'/update/')}}" class="btn btn-primary align-self-center mb-1">Letöltés</a>   
+                    </div>
+                </div>
             </div>
-            <a href="{{ url('/events/'. $item->id) }}" class="stretched-link"></a>
+            
+            
+            <div class="d-flex justify-content-center">
+                <div class="card d-flex mb-3 col-sm-10 align-self-center">
+                    <h5 class="card-header">{{ $item->name }}</h5>
+                    <div class="card-body d-flex align-items-center justify-content-center flex-row">
+                            <div class="d-flex">
+                                <label for="date" class="d-flex align-self-baseline"><h6>Rendezvény ideje:&nbsp</h6></label>
+                                <div name="date" class="d-flex align-self-baseline">
+                                    {{ $item->date }}
+                                </div>
+                            </div>
+                        </div>
+                        <a href="{{ url('/events/'. $item->id) }}" class="stretched-link"></a>
+                    </div>
+            </div>
+            
         </div>
+        
+        
     @endforeach
 </div>
     
