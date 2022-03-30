67bb3056822292260427e19bd9e985098c7c0c09
bPet75 <bpeti437@gmail.com>
2022-03-30 14:13:25 +0200

    Merge hibáinak javítása

--- App/SportCheck/routes/web.php
+++ App/SportCheck/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\Event_User_StatusController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExcelController;
 use Illuminate\Support\Facades\Auth;
 
