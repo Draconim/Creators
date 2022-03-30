4f7815a8c0f85b8f3f1c87cca02e95ebd63bb4e7
bPet75 <bpeti437@gmail.com>
2022-03-30 13:38:41 +0200

    Route bővítés

--- App/SportCheck/routes/web.php
+++ App/SportCheck/routes/web.php
@@ -58,3 +58,7 @@ Route::post('/events/{id}/update', [EventController::class, 'update']);
 //Route::get('/events', [EventController::class, 'index'])->name('eventlist');
 //Route::get('/events/{id}', [EventController::class, 'show'])->name('details_event');
 Route::get('exportExcel',[ExcelController::class, 'exportExcel'])->name('export');
+Route::get('/users', [UserController::class, 'index'])->name('userlist');
+Route::get('/users/search', [UserController::class, 'search'])->name('usersearch');
+Route::get('/users/{id}/toadmin', [USerController::class, 'update'])->name('user_toadmin');
+
