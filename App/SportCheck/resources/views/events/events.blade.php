2beb1998ec0e45e8ebb3957cc4e2befcef870843
Barankai László Ferenc <barankailaci@gmail.com>
2022-04-12 11:58:13 +0200

    Event listában kártya módosítások, description belerakása

--- App/SportCheck/resources/views/events/events.blade.php
+++ App/SportCheck/resources/views/events/events.blade.php
@@ -6,9 +6,9 @@
     <a href="{{route('create_event')}}" class="d-flex align-self-center justify-content-center btn btn-primary btn-m col-sm-2 m-4 text-nowrap"> Új rendezvény hozzáadása</a>
     @endif
     @foreach($events as $item)
-        <div class="d-flex flex-column">
+        <div class="d-flex flex-column mx-2">
             <div class="d-flex justify-content-center">
-                <div class="card d-flex mb-3 col-sm-10 align-self-center">
+                <div class="card d-flex mb-3  col-sm-10 col-lg-5 align-self-center">
                     <div class="card-header d-flex flex-row align-items-center justify-content-between">
                         <div class="d-flex w-25"></div>
                         <div class="d-flex"><h5>{{ $item->name }}</h5></div>
@@ -27,7 +27,7 @@
                                     </div>
                                     <div class="d-flex">
                                         <div class="d-flex mx-2">
-                                            <a href="{{url('/events/'.$item->id.'/qr/set/')}}" class="btn btn-primary align-self-center mb-1">QR kód lekérése</a>   
+                                            <a href="{{url('/events/'.$item->id.'/qr/set/')}}" class="btn btn-info align-self-center mb-1">QR kód</a>   
                                         </div>
                                     </div>
                                     <form action="{{url('/events')}}" method="POST">
@@ -45,16 +45,18 @@
                         </div>
                     </div>
                     <a href="{{ url('/events/'. $item->id) }}" class="text-reset text-decoration-none">
-                    <div class="card-body d-flex align-items-center justify-content-center flex-row ">
-                            <div class="d-flex">
-                                <label for="date" class="d-flex align-self-baseline"><h6>Rendezvény ideje:&nbsp</h6></label>
-                                <div name="date" class="d-flex align-self-baseline">
-                                    {{ $item->date }}
-                                </div>
+                    <div class="card-body d-flex align-items-center justify-content-center flex-column ">
+                    <div class="d-flex">
+                        {{$item->description}}
+                    </div>
+                        <div class="d-flex flex-row mt-2">
+                            <label for="date" class="d-flex align-self-baseline"><h6>Rendezvény ideje:&nbsp</h6></label>
+                            <div name="date" class="d-flex align-self-baseline">
+                                {{ $item->date }}
                             </div>
                         </div>
                     </div>
-                </a>
+                </div>
             </div>
         </div>
     @endforeach
--- App/SportCheck/resources/views/qr.blade.php
+++ App/SportCheck/resources/views/qr.blade.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
     <title>Document</title>
 </head>
 <body>
-    Esemény neve: {{$event->name}}<br>
-    Esemény leírása: {{$event->description}}<br>
-    {{QrCode::generate("http://127.0.0.1:8000/events/checkin/".$event->code);}}
+    <table>
+        <th><h3>{{$event->name}}</h3></th>
+        <tr>
+            <td><h3>Esemény leírása:</h3></td>
+        </tr>
+        <tr>
+            <td><div style="font-size:25px">{{$event->description}}</div></td>
+        </tr>
+        <th>
+            <div style="margin-top:30px">
+                {{QrCode::size(420)->generate("http://127.0.0.1:8000/events/checkin/".$event->code);}}
+            </div>
+        </th>
+    </table>
+    
 </body>
 </html>
